49a2b79107d2a528cec1725f51442ca9fdd2b031
Antonio <antoniobust@gmail.com>
2020-05-03 14:34:52 +0100

    Build folder deleted

--- Build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/parallels/Workspace/CppND-Route-Planning-Project/thirdparty/googletest/googletest
-# Build directory: /home/parallels/Workspace/CppND-Route-Planning-Project/Build/thirdparty/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
